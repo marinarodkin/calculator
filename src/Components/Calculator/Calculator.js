1d4d1b66a69bfd7c5aa1ecfddaf09c37e507a0d6
marinarodkin <marina.rodkina1@gmail.com>
2019-03-06 14:55:27 +0100

    Init

---
+++ src/Components/Calculator/Calculator.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+import Display from '../Display/Display';
+import Keypad from '../Keypad/Keypad';
+
+class Calculator extends Component {
+  state = {
+    // value to be displayed in <Display />
+    displayValue: '0',
+    // values to be displayed in number <Keys />
+    numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0','ce'],
+    // values to be displayed in operator <Keys />
+    operators: ['/', 'x', '-', '+'],
+    // operator selected for math operation
+    selectedOperator: '',
+    // stored value to use for math operation
+    storedValue: '',
+    isNewNumber: false,
+    isAfterResult: false,
+  }
+
+
+    callOperator = () => {
+      let {displayValue, selectedOperator, storedValue} = this.state;
+      // temp variable for updating state storedValue
+      const updateStoredValue = displayValue;
+
+      // parse strings for operations
+      //displayValue = parseInt(displayValue, 10);
+      //storedValue = parseInt(storedValue, 10);
+      displayValue = +displayValue;
+      storedValue = +storedValue;
+
+
+      // performs selected operation
+      switch (selectedOperator) {
+        case '+':
+          displayValue = storedValue + displayValue;
+          break;
+        case '-':
+          displayValue = storedValue - displayValue;
+          break;
+        case 'x':
+          displayValue = storedValue * displayValue;
+          break;
+        case '/':
+          displayValue = storedValue / displayValue;
+          break;
+        default:
+          // set displayValue to zero if no case matches
+          displayValue = '0';
+      }
+      // converts displayValue to a string
+      if (displayValue > 999999999)  displayValue = "too large";
+      displayValue = displayValue.toString();
+
+      // reset selectedOperator
+      selectedOperator = '';
+      // check for 'NaN' or 'Infinity', if true set displayValue to '0'
+      if (displayValue === 'NaN' || displayValue === 'Infinity') displayValue = '0';
+
+      this.setState({ displayValue, selectedOperator, storedValue: updateStoredValue, isNewNumber: true, isAfterResult: true });
+    }
+
+
+
+  setOperator = value => {
+
+    let { displayValue, selectedOperator, storedValue } = this.state;
+    if(displayValue === "too large") return;
+    // check if a value is already present for selectedOperator
+    if (selectedOperator === '') {
+      // update storedValue to the value of displayValue
+      storedValue = displayValue;
+      // reset the value of displayValue to '0'
+      //***displayValue = '0';
+      // update the value of selectedOperator to the given value
+      selectedOperator = value;
+    } else {
+      // if selectedOperator is not an empty string
+      // update the value of selectedOperator to the given value
+      selectedOperator = value;
+    }
+
+    this.setState({ displayValue, selectedOperator, storedValue, isNewNumber: true });
+  }
+
+  updateDisplay = value => {
+    let { displayValue,  isNewNumber, isAfterResult } = this.state;
+
+    // prevent multiple occurences of '.'
+    if (value === '.' && displayValue.includes('.')) value = '';
+
+    if (value === 'ce') {
+      //if after clean display
+      if (isAfterResult === true) displayValue = '0';
+      // deletes last char in displayValue
+      displayValue = displayValue.substr(0, displayValue.length - 1);
+      // set displayValue to '0' if displayValue is empty string
+      if (displayValue === '') displayValue = '0';
+
+    } else {
+      // ***if the first number after operator -
+      // replace displayValue with value if displayValue equal to '0'
+      // else concatenate displayValue and value
+
+      if (displayValue === '0' || isNewNumber === true || displayValue === "too large") {
+        displayValue = value
+      } else {
+        displayValue += value;
+      }
+    }
+
+    this.setState({ displayValue, isNewNumber: false, isAfterResult: false });
+  }
+
+  render = () => {
+    // unpack the component state by using Object Destructuring
+    const { displayValue, numbers, operators } = this.state;
+
+    return (
+      <div className="calculator-container">
+        <Display displayValue={displayValue} />
+        <Keypad
+          callOperator={this.callOperator}
+          numbers={numbers}
+          operators={operators}
+          setOperator={this.setOperator}
+          updateDisplay={this.updateDisplay}
+        />
+      </div>
+    );
+  }
+}
+
+export default Calculator;
